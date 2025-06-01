aa0d01580a26be51039856650452e6aa2c3a8bdd
Richard Pritchett <28760346+freetcp@users.noreply.github.com>
2025-06-01 11:35:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -333,6 +333,16 @@ async function processXray(data, index) {
   }
 }
 
+async function getPhysicalLocation(address) {
+  try {
+    const geo = geoip.lookup(address);
+    const country = geo ? geo.country : "CloudFlare";
+    return `${country} ${getFlagEmoji(country)}`;
+  } catch (e) {
+    console.error(`区域代码获取失败: ${e}`);
+    return "";
+  }
+}
 
 function getFlagEmoji(countryCode) {
   // 将国家/地区代码转换为大写，以匹配 ISO 3166-1 alpha-2 格式
@@ -340,12 +350,12 @@ function getFlagEmoji(countryCode) {
 
   // 计算国旗 emoji 的 Unicode 编码
   // 每个国家/地区的 Unicode 编码范围是从 'A' 到 'Z'，并且与国家/地区代码对应
-  const unicodeOffset = 127397; // 国旗 emoji 的 Unicode 偏移量
-  const firstLetter = countryCodeUpper.charCodeAt(0);
-  const secondLetter = countryCodeUpper.charCodeAt(1);
-  const emoji = String.fromCodePoint(firstLetter + unicodeOffset, secondLetter + unicodeOffset);
+  // const unicodeOffset = 127397; // 国旗 emoji 的 Unicode 偏移量
+  // const firstLetter = countryCodeUpper.charCodeAt(0);
+  // const secondLetter = countryCodeUpper.charCodeAt(1);
+  // const emoji = String.fromCodePoint(firstLetter + unicodeOffset, secondLetter + unicodeOffset);
 
-  return emoji;
+  // return emoji;
 }
 
 async function writeClashMetaProfile(templateFile, outputFile, extractedProxies) {
